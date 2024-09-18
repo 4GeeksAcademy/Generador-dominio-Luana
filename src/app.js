561da4edbf15f4f458fd1d14610679817f897561
Luana Tajes <lmluana27@gmail.com>
2024-09-18 14:47:28 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -8,4 +8,19 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let ext = ["com", "uy"];
+
+  for (let i = 0; i < pronoun.length; i++) {
+    // console.log(pronoun[i]);
+    for (let a = 0; a < adj.length; a++) {
+      for (let n = 0; n < noun.length; n++) {
+        for (let e = 0; e < ext.length; e++) {
+          console.log(`${pronoun[i]}.${adj[a]}${noun[n]}.${ext[e]}`);
+        }
+      }
+    }
+  }
 };
