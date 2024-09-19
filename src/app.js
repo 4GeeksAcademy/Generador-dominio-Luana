12f780d41759953f250b5ef6b9558a078028eb01
Luana Tajes <lmluana27@gmail.com>
2024-09-19 00:08:34 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,26 +1,23 @@
 /* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon"];
-  let ext = ["com", "uy"];
+//write your code here
+console.log("Hello Rigo from the console!");
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon", "puedes"];
+let ext = ["com", "uy", "es"];
 
-  for (let i = 0; i < pronoun.length; i++) {
-    // console.log(pronoun[i]);
-    for (let a = 0; a < adj.length; a++) {
-      for (let n = 0; n < noun.length; n++) {
-        for (let e = 0; e < ext.length; e++) {
-          console.log(`${pronoun[i]}.${adj[a]}${noun[n]}.${ext[e]}`);
+for (let i = 0; i < pronoun.length; i++) {
+  // console.log(pronoun[i]);
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for (let e = 0; e < ext.length; e++) { 
+        if (noun[n].endsWith(ext[e])){
+          console.log(`${pronoun[i]}${adj[a]}${noun[n].slice(0, -ext[e].length)}.${ext[e]}`);
         }
+        console.log(`${pronoun[i]}.${adj[a]}${noun[n]}.${ext[e]}`);
       }
     }
   }
-};
+}
